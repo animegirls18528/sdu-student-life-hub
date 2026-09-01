9a2c9b86ac345dfef16bc11642b98d28f37cc8ef
nitit828@gmail.com <animegirls828@gmail.com>
2026-09-01 15:15:53 +0700

    Fix login error: switch bcrypt to bcryptjs for Netlify compatibility

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,5 @@
 import { SignJWT, jwtVerify } from 'jose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 import crypto from 'crypto';
